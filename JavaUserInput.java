3348b92ff33e85f97fa625857cf4f19aa3991c16
PatelDivyesh <patel.divyesh123@gmail.com>
2025-08-20 22:45:45 +0530

    Added new programs

---
+++ JavaUserInput.java
@@ -0,0 +1,15 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class JavaUserInput {
+    public static void main(String[] args) {
+        Scanner objScanner = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+    
+        String userName = objScanner.nextLine();
+        System.out.println("Your Name is: " + userName);
+
+        objScanner.close();
+    }
+}
